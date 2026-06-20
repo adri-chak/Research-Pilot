7ac2008a806b7a745456f6329d75b3d3f889f46b
adri-chak <adrija.chakraborty2024@iem.edu.in>
2026-06-20 22:52:24 +0530

    add project review agent

---
+++ backend/project_reviewer.py
@@ -0,0 +1,40 @@
+from models.project_review import ProjectReview
+from dotenv import load_dotenv
+import os
+
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+structured_llm = llm.with_structured_output(ProjectReview)
+
+
+def review_project(project):
+    prompt = f"""
+    Review the following student project idea.
+
+    Project:
+    {project}
+
+    Return:
+    - strengths
+    - weaknesses
+    - score out of 10
+    """
+
+    response = structured_llm.invoke(prompt)
+
+    return response
+
+result = review_project(
+    "AI-Powered Medical Diagnosis System"
+)
+
+print(result.strengths)
+print(result.weaknesses)
+print(result.score)
